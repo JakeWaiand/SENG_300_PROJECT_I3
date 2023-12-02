58a38345a8f4b1499b5c6ae694a83449047a89ea
Code_with_Moon <tahaminachy43@gmail.com>
2023-12-02 16:08:53 -0700

    Creating StartSession Panel

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/StartSessionPanel.java
@@ -0,0 +1,12 @@
+package com.thelocalmarketplace.software.gui;
+import javax.swing.JFrame;
+
+public class StartSessionPanel {
+    public StartSessionPanel(StartSession session ){
+        JFrame frame = new JFrame(); // Creates a frame 
+
+        frame.setVisible(true);
+        frame.setSize(420,420);// sets dimensions
+    }
+    
+}
