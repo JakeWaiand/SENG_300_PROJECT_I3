0e896d5e6b5fe4923ccd888f7eb06df39857e6f4
FardinSami75 <149439557+FardinSami75@users.noreply.github.com>
2023-12-04 16:57:39 -0700

    Create DataCollector.java

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/DataCollector.java
@@ -0,0 +1,32 @@
+public class DataCollector {
+
+    private MaintainInk inkMaintainer;
+    private MaintainBanknotes banknoteMaintainer;
+    private MaintainCoins coinMaintainer;
+    private MaintainPaper paperMaintainer;
+
+    public DataCollector() {
+        inkMaintainer = new MaintainInk();
+        banknoteMaintainer = new MaintainBanknotes();
+        coinMaintainer = new MaintainCoins();
+        paperMaintainer = new MaintainPaper();
+    }
+
+    // Existing methods
+    public List<InkUsageData> collectInkUsageData() {
+        // Implementation
+    }
+
+    public List<PaperUsageData> collectPaperUsageData() {
+        // Implementation
+    }
+
+    // New methods to integrate other classes
+    public List<BanknoteData> collectBanknoteData() {
+        // Utilize banknoteMaintainer to collect banknote data
+    }
+
+    public List<CoinData> collectCoinData() {
+        // Utilize coinMaintainer to collect coin data
+    }
+}
